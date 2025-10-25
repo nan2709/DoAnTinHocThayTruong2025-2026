7695479d050baa74d8e6eb1f6304024bd13d3bcb
hoangak152 <hoangakpro1521987@gmail.com>
2025-10-25 21:13:43 +0700

    em nop file doc ghi

---
+++ DoAnTinHoc2025-2026/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace CSV_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Dọn dẹp tài nguyên đang dùng.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Hàm khởi tạo giao diện Form.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 80);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(720, 400);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(15, 20);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(129, 40);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Đọc CSV";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(587, 20);
+            this.btnWrite.Margin = new System.Windows.Forms.Padding(4);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(129, 40);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "Ghi CSV";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 508);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Đọc / Ghi CSV - Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
---
+++ DoAnTinHoc2025-2026/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CSV_Demo
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "data.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Không tìm thấy file data.csv trong thư mục Debug!");
+                return;
+            }
+
+            DataTable dt = new DataTable();
+            string[] lines = File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                // Tạo cột từ dòng đầu tiên
+                string firstLine = lines[0];
+                string[] headerLabels = firstLine.Split(',');
+                foreach (string header in headerLabels)
+                {
+                    dt.Columns.Add(header);
+                }
+
+                // Thêm dữ liệu các dòng còn lại
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] dataWords = lines[i].Split(',');
+                    dt.Rows.Add(dataWords);
+                }
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.DataSource == null)
+            {
+                MessageBox.Show("Không có dữ liệu để ghi!");
+                return;
+            }
+
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            var lines = new string[dt.Rows.Count + 1];
+
+            // Ghi header
+            string[] columnNames = dt.Columns.Cast<DataColumn>()
+                                             .Select(column => column.ColumnName)
+                                             .ToArray();
+            lines[0] = string.Join(",", columnNames);
+
+            // Ghi từng dòng dữ liệu
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string[] fields = dt.Rows[i].ItemArray.Select(field => field.ToString()).ToArray();
+                lines[i + 1] = string.Join(",", fields);
+            }
+
+            File.WriteAllLines(filePath, lines);
+            MessageBox.Show("Ghi file CSV thành công!");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
